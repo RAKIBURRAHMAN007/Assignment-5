0ee44524a13489f44ba1681eec4757e3353bbd26
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2024-09-24 12:30:54 +0600

    history part complete

--- button.js
+++ button.js
@@ -1,9 +1,15 @@
 document.getElementById('btn-donation').addEventListener('click',function(){
     showSectionById('donation-section');
+    document.getElementById('btn-donation').style.backgroundColor='#B4F461';
+    document.getElementById('btn-history').style.backgroundColor='white';
 
 });
 document.getElementById('btn-history').addEventListener('click',function(){
-    showSectionById('btn-history');
+    showSectionById('history-section');
+    document.getElementById('btn-history').style.backgroundColor='#B4F461';
+    document.getElementById('btn-donation').style.backgroundColor='white';
+    document.getElementById('foot').style.marginTop='300px';
+    
     
 
 })
--- donation.js
+++ donation.js
@@ -6,17 +6,44 @@ document.getElementById('noakhali-donation-btn').addEventListener('click',functi
     const currentNoakhaliBalance=orginalNoakhaliBalance+donationAmount;
 
     const currentBalance=orginalBalance-donationAmount;
-    if(donationAmount>currentBalance || donationAmount<0 ){
-        alert('you canot donate');
+
+    if(donationAmount>orginalBalance || donationAmount<0 ){
+        alert('You Canot Donate,Insufficient Balance.');
         return;
     }
     if(isNaN(donationAmount)){
-        alert('enter valid amount');
+        alert('Enter Valid Amount');
         return;
     }
     
     document.getElementById('main-balance').innerText=currentBalance;
     document.getElementById('noakhali-amount').innerText=currentNoakhaliBalance;
+    const p=document.createElement('p');
+    p.innerText=`${donationAmount}Taka is Donated for Flood Relief in Noakhali,Bangladesh`;
+
+    
+    const card = document.createElement('div');
+    card.style.border = '1px solid #D3D3D3'; 
+    card.style.borderRadius = '10px'; 
+    card.style.padding = '50px';
+    card.style.margin = '10px '; 
+    
+
+   
+    const donationParagraph = document.createElement('p');
+    donationParagraph.innerText = `${donationAmount} Taka is donated for flood relief in Noakhali, Bangladesh.`;
+    donationParagraph.style.fontSize='30px';
+    donationParagraph.style.fontWeight='700px';
+    card.appendChild(donationParagraph); 
+    const dateString = new Date().toString(); 
+    const dateParagraph = document.createElement('p');
+    dateParagraph.innerText =`Date : ${dateString}`;
+    
+    dateParagraph.style.fontSize='17px';
+    
+    card.appendChild(dateParagraph);
+    document.getElementById('history-container').appendChild(card);
+
     my_modal_1.showModal();
 })
 
@@ -29,17 +56,34 @@ document.getElementById('feni-donation-btn').addEventListener('click',function(e
     const currentFeniBalance=orginalFeniBalance+donationAmount;
 
     const currentBalance=orginalBalance-donationAmount;
-    if(donationAmount>currentBalance || donationAmount<0){
-        alert('you canot donate');
+    if(donationAmount>orginalBalance || donationAmount<0){
+        alert('You Canot Donate,Insufficient Balance.');
         return;
     }
     if(isNaN(donationAmount)){
-        alert('enter valid amount');
+        alert('Enter Valid Amount');
         return;
     }
     
     document.getElementById('main-balance').innerText=currentBalance;
     document.getElementById('feni-amount').innerText=currentFeniBalance;
+    const card2=document.createElement('div');
+    card2.style.border='1px solid #D3D3D3'
+    card2.style.borderRadius='10px';
+    card2.style.padding='50px';
+    card2.style.margin='10px'
+    const donationParagraph=document.createElement('p');
+    donationParagraph.innerText=`${donationAmount} Taka is Donated for Flood Relief in Feni,Bangladesh`;
+    donationParagraph.style.fontSize='30px';
+    donationParagraph.style.fontWeight='700px'
+    card2.appendChild(donationParagraph);
+
+    const dateString=new Date().toString();
+    const dateParagraph=document.createElement('p');
+    dateParagraph.innerText=`Date : ${dateString}`;
+    card2.appendChild(dateParagraph);
+    document.getElementById('history-container').appendChild(card2);
+
     my_modal_2.showModal();
 })
 // quota movement donate
@@ -52,17 +96,33 @@ document.getElementById('quota-donate-btn').addEventListener('click',function(ev
     const currentQuotaBalance=orginalQuotaBalance+donationAmount;
 
     const currentBalance=orginalBalance-donationAmount;
-    if(donationAmount>currentBalance || donationAmount<0){
-        alert('you canot donate');
+    if(donationAmount>orginalBalance || donationAmount<0){
+        alert('You Canot Donate,Insufficient Balance.');
         return;
     }
     if(isNaN(donationAmount)){
-        alert('enter valid amount');
+        alert('Enter Valid Amount');
         return;
     }
     
     document.getElementById('main-balance').innerText=currentBalance;
     document.getElementById('quota-amount').innerText=currentQuotaBalance;
+    const card3=document.createElement('div');
+    card3.style.border='1px solid #D3D3D3'
+    card3.style.borderRadius='10px';
+    card3.style.padding='50px';
+    card3.style.margin='10px'
+    const donationParagraph=document.createElement('p');
+    donationParagraph.innerText=`${donationAmount} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh`;
+    donationParagraph.style.fontSize='30px';
+    donationParagraph.style.fontWeight='700px'
+    card3.appendChild(donationParagraph);
+
+    const dateString=new Date().toString();
+    const dateParagraph=document.createElement('p');
+    dateParagraph.innerText=`Date : ${dateString}`;
+    card3.appendChild(dateParagraph);
+    document.getElementById('history-container').appendChild(card3);
     my_modal_3.showModal();
     
 })
